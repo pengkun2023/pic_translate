093047bedb24d69e0f0558112e264f0a7172109f
pengkun2023 <zhangpk2023@163.com>
2026-05-08 18:19:42 +0800

    Remove specific files from tracking

--- test_response.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-import os
-os.environ['http_proxy'] = ''
-os.environ['https_proxy'] = ''
-os.environ['all_proxy'] = ''
-client = OpenAI(api_key="sk-906b97091638488f86988b15926e810b", base_url="https://api.deepseek.com")
-print(client.base_url)
